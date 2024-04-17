5382fe8c7342676178c3e1e51f13b8e671e32b06
Biplove <93914620+Biplove82@users.noreply.github.com>
2024-04-17 15:03:07 +0530

    com

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,38 +1,26 @@
-const userModels = require("../Modells/userModells");
+const userModells = require("../Modells/userModells");
+
 const registration = async function (req, res) {
   try {
-    const { firstName, lastName, email, message } = req.body;
-
-
-    const existingUser = await userModels.findOne({ email:email });
-
-    if (existingUser) {
-      return res.status(401).json({ message: "Username already exists" });
-    }
-    const registerData = new userModels({
+  const { firstName, lastName, email, message } = req.body;
+  const registerData = new userModells({
       firstName,
       lastName,
       email,
       message,
     });
-
     await registerData.save();
-
-
     res
       .status(200)
       .json({ id: registerData._id, msg: "User Registered Succesfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" + error });
   }
-
-
 };
-  
-const getData = async function(req,res){
-  let data= await userModels.find();
-  res.status(200).json({data})
 
-}
+const getData = async function (req, res) {
+  let data = await userModells.find();
+  res.status(200).json({ data });
+};
 
-module.exports = { registration,getData };
+module.exports = { registration, getData };
